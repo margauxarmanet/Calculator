a7c454a53bf729197adaece504e4532e96baaf1b
Margaux Armanet <112400605+margauxarmanet@users.noreply.github.com>
2023-03-03 12:00:52 +0100

    Update main.js
    - added keyboard support.

--- js/main.js
+++ js/main.js
@@ -3,26 +3,54 @@ const initApp = () => {
   const currentValueElement = document.querySelector(".currentValue");
   const previousValueElement = document.querySelector(".previousValue");
 
-  // Writes the user's (numbers) input into the "results window".
+  // -------------------------------------------------------------------
+  // Listening for mouse number input.
   const numberButtons = document.querySelectorAll(".number");
   numberButtons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-      /* event.target.textContent accesses the text content 
-      of the html element that triggered the click event.*/
-      const newValueInput = event.target.textContent;
-
-      // Erases the default value (0) before displaying the user's input
-      if (currentValueElement.value === "0") {
-        currentValueElement.value = newValueInput;
-      } else {
-        currentValueElement.value += newValueInput;
-      }
-    });
+    button.addEventListener("click", handleNumberClick);
   });
 
-  // The "delete button" removes the last character entry.
+  // Listening for keyboard number input.
+  document.addEventListener("keydown", (event) => {
+    const keyValue = event.key;
+
+    if (/\d/.test(keyValue)) {
+      event.preventDefault();
+      handleNumberClick({ target: { textContent: keyValue } });
+    }
+  });
+
+  // Writes the user's (numbers) input into the "results window".
+  function handleNumberClick(event) {
+    /* event.target.textContent accesses the text content 
+      of the html element that triggered the click event.*/
+    const newValueInput = event.target.textContent;
+
+    // Erases the default value (0) before displaying the user's input
+    if (currentValueElement.value === "0") {
+      currentValueElement.value = newValueInput;
+    } else {
+      currentValueElement.value += newValueInput;
+    }
+  }
+
+  // -------------------------------------------------------------------
+  // Listening for mouse click on "backspace".
   const backspaceButton = document.querySelector(".backspace");
-  backspaceButton.addEventListener("click", () => {
+  backspaceButton.addEventListener("click", clickBackspaceButton);
+
+  // Listening for keyboard "backspace".
+  document.addEventListener("keydown", (event) => {
+    const keyValue = event.key;
+
+    if (keyValue === "Backspace") {
+      event.preventDefault();
+      backspaceButton.click();
+    }
+  });
+
+  // The "backspace button" removes the last character entry.
+  function clickBackspaceButton() {
     // if there is only one character left, it resets to 0.
     if (currentValueElement.value.length === 1) {
       currentValueElement.value = "0";
@@ -32,14 +60,29 @@ const initApp = () => {
     } else {
       currentValueElement.value = currentValueElement.value.slice(0, -1);
     }
+  }
+
+  // -------------------------------------------------------------------
+  //  Listening for mouse click on "CE".
+  const clearEntryButton = document.querySelector(".clearEntry");
+  clearEntryButton.addEventListener("click", clearEntry);
+
+  // Listening for keyboard "delete" (CE).
+  document.addEventListener("keydown", (event) => {
+    const keyValue = event.key;
+
+    if (keyValue === "Delete") {
+      event.preventDefault();
+      clearEntryButton.click();
+    }
   });
 
   // The "CE" button resets the entry to 0.
-  const clearEntryButton = document.querySelector(".clearEntry");
-  clearEntryButton.addEventListener("click", () => {
+  function clearEntry() {
     currentValueElement.value = "0";
-  });
+  }
 
+  // -------------------------------------------------------------------
   // The "C" button resets everything to 0.
   const clearButton = document.querySelector(".clear");
   clearButton.addEventListener("click", () => {
@@ -53,26 +96,54 @@ const initApp = () => {
     currentValueElement.value = parseFloat(currentValueElement.value) * -1; // parseFloat() turns the string into a number.
   });
 
-  // Adds the symbols ("+", "-", "*", "/" and ".") and don't repeat themselves.
+  // -------------------------------------------------------------------
+  // Listening for mouse operator input.
   const operatorButtons = document.querySelectorAll(".operator");
   operatorButtons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-      const newOperatorInput = event.target.textContent;
-      const lastChar = currentValueElement.value.slice(-1);
-      if (
-        lastChar !== "+" &&
-        lastChar !== "-" &&
-        lastChar !== "*" &&
-        lastChar !== "/" &&
-        lastChar !== "."
-      )
-        currentValueElement.value += newOperatorInput;
-    });
+    button.addEventListener("click", handleOperatorClick);
+  });
+
+  // Listening for keyboard operator input.
+  document.addEventListener("keydown", (event) => {
+    const keyValue = event.key;
+    if (/[-+*/.]/.test(keyValue)) {
+      event.preventDefault();
+      handleOperatorClick({ target: { textContent: keyValue } });
+    }
+  });
+
+  // Adds the symbols ("+", "-", "*", "/" and ".").
+  // Also makes sure that a symbol cannot be preceded by another.
+  function handleOperatorClick(event) {
+    const newOperatorInput = event.target.textContent;
+    const lastChar = currentValueElement.value.slice(-1);
+    if (
+      lastChar !== "+" &&
+      lastChar !== "-" &&
+      lastChar !== "*" &&
+      lastChar !== "/" &&
+      lastChar !== "."
+    )
+      currentValueElement.value += newOperatorInput;
+  }
+
+  // -------------------------------------------------------------------
+  // Listening for mouse click on "equals".
+  const equalsButton = document.querySelector(".equals");
+  equalsButton.addEventListener("click", calculateResult);
+
+  // Listening for keyboard "Enter".
+  document.addEventListener("keydown", (event) => {
+    const keyValue = event.key;
+
+    if (keyValue === "Enter") {
+      event.preventDefault();
+      equalsButton.click();
+    }
   });
 
   // The equals button calculates and displays the result.
-  const equalsButton = document.querySelector(".equals");
-  equalsButton.addEventListener("click", () => {
+  function calculateResult() {
     const expression = currentValueElement.value;
 
     // Checks for division by zero.
@@ -87,8 +158,11 @@ const initApp = () => {
     const result = math.evaluate(expression); // math.evaluate() is from the js library math.js
     previousValueElement.textContent = expression + " =";
     currentValueElement.value = result;
-  });
+  }
 };
 
 // Makes sure the page is fully loaded.
 document.addEventListener("DOMContentLoaded", initApp);
+
+// add keyboard support to readme file on github.
+// replace with icons : visually have font awesome icons but background is the operators: + - * / .
